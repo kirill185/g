a83302553d3021a0ba75e38fc40a223d37d09085
Kirill185 <Bogatovkirill799@gmail.com>
2025-10-09 10:58:19 +0500

    fds

---
+++ asdasd/class/BaseClass/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdasd.Class.BaseClass
+{
+    public class Сar
+    {
+        private int Id;
+        internal string Name;
+        protected string Model;
+        public DateTime CreateDate;
+        internal protected DateTime UseDate;
+
+    }
+}
+
---
+++ asdasd/class/BaseClass/van.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asdasd.Class.BaseClass;
+
+namespace asdasd.Class.BaseClass
+{
+    public class Van : Сar
+    
+    {
+        void test()
+        {
+
+        }
+    }
+}
