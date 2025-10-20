58ded9e7c7fc00d8af9864c1e732c3276f8c0130
Kirill185 <Bogatovkirill799@gmail.com>
2025-10-20 10:57:10 +0500

    upd

---
+++ 98498+/SQLQuery3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[students](
+		[id]		[BIGINT] identity (1,1),
+		[name]	[nvarchar](50),
+		[country]	[nvarchar](50),
+		[gender]	[bit],
+		)
+
---
+++ 98498+/SQLQuery5.sql
@@ -0,0 +1,12 @@
+000000004444440select * from [dbo].[students]
+insert into [dbo].[students]
+		([name], [Country], [gender])
+values ('myhamed','ukraina', 1)
+
+
+--delete from [dbo].[students]
+	--where id = 
+
+update [dbo].[students]
+	set [country]='russia'
+where [name]='mona'
